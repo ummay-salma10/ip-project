2ec4a440a6b38d9824d8c9d2deffc22e9a8bc180
Ummay Salma <tajinsultana698@gmail.com>
2026-09-24 14:45:01 +0600

    Add files via upload

---
+++ javascript-practice/script.js
@@ -0,0 +1,12 @@
+        let name = "Ummay Salma";
+        let age = 22;
+        let department = "CSE";
+        let university = "Premier University Chittagone";
+
+        //display information
+        console.log("Name: ",name);
+        console.log("age: ", age);
+        console.log("Department: ", department);
+        console.log("University: ", university);
+
+        
